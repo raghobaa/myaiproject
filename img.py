10424e227631f3c057c032c032690c51bca51e35
raghobaa <160655306+raghobaa@users.noreply.github.com>
2026-07-17 19:20:05 +0530

    Update img.py

--- img.py
+++ img.py
@@ -4,10 +4,10 @@ import google.generativeai as ai
 import os
 import matplotlib.pyplot as plt
 
-image_path = "/Users/raghav/Desktop/untitled folder/pexels-anjana-c-169994-674010.jpg" 
+image_path = image_path
 img = Image.open(image_path)
 plt.imshow(img)
-ai.configure(api_key="AIzaSyCHGvCV_UsrQLx8EZrb58IQ9qqQEyRNcYI")  
+
 model = ai.GenerativeModel(model_name="gemini-1.5-pro")
 response=model.generate_content([img,"describe the image"])
 print(response.text)
